19ea54a5ef2d5801c595b864a5fb2f3224147f87
Kostas Ioannou <kostazj@gmail.com>
2019-01-24 13:03:21 +0200

    Changed the title of the bad request error

--- src/errors/bad-request.error.ts
+++ src/errors/bad-request.error.ts
@@ -1,7 +1,7 @@
 import { BasicError } from './basic.error';
 
 export class BadRequestError extends BasicError {
-  constructor(message: string, code: number = 1000, title: string = 'Authentication Error') {
+  constructor(message: string, code: number = 1000, title: string = 'Bad Request Error') {
     super(message, code, 400, title);
   }
 }
